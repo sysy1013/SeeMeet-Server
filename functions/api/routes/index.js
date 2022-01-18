94db6e65f1c6698da917283edbb9e63e8561435b
gunom <gu3062@naver.com>
2022-01-18 15:48:21 +0900

    [Fix] : friend 라우터 수정

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use('/plan', require('./plan'));
 router.use('/invitation', require('./invitation'));
 router.use('/invitation-response', require('./invitationResponse'));
-
+router.use('/friend', require('./friend'));
 router.use('/auth', require('./auth'));
 
 module.exports = router;
