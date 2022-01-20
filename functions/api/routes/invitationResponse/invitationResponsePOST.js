b989db83a875e07cc2e01e3d1811d5790340a84f
gunom <gu3062@naver.com>
2022-01-20 18:10:12 +0900

    [Add] : 초대장 응답 webhook 추가

--- functions/api/routes/invitationResponse/invitationResponsePOST.js
+++ functions/api/routes/invitationResponse/invitationResponsePOST.js
@@ -5,12 +5,21 @@ const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const jwtHandlers = require('../../../lib/jwtHandlers');
 const { invitationResponseDB, invitationDB } = require('../../../db');
+const { send } = require('../../../lib/slack');
 
 module.exports = async (req, res) => {
   const { invitationId } = req.params;
   const { invitationDateIds } = req.body;
   const { accesstoken } = req.headers;
-  if (!invitationId || !invitationDateIds) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  if (!invitationId || !invitationDateIds || !accesstoken) {
+    await send(`
+    req.originalURL: ${req.originalUrl}
+    invitationId: ${invitationId}
+    invitationDateIds: ${invitationDateIds}
+    accesstoken: ${accesstoken}
+    `);
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
 
   let client;
   const decodedToken = jwtHandlers.verify(accesstoken);
@@ -19,8 +28,19 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
     const invitation = await invitationDB.getInvitationById(client, invitationId);
-    if (!invitation) return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_INVITATION));
+    if (!invitation) {
+      await send(`
+      req.originalURL: ${req.originalUrl}
+      invitation: ${JSON.stringify(invitation)}
+      `);
+      return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NO_INVITATION));
+    }
     if (invitation.isConfirmed || invitation.isCancled) {
+      await send(`
+      req.originalURL: ${req.originalUrl}
+      isConfirmed: ${invitation.isConfirmed}
+      isCancled: ${invitation.isCancled}
+    `);
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_CONFIRM));
     }
     const data = await invitationResponseDB.responseInvitation(client, userId, invitationId, invitationDateIds);
@@ -29,6 +49,7 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+    await send(error);
 
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
